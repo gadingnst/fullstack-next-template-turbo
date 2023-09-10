7530707d81591a35f40d195bc85e0e074589f4c9
Gading Nasution <account@gading.dev>
2023-09-10 17:10:51 +0700

    init eslint & rules

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,11 +1,12 @@
-import Image from "next/image";
-import { Card } from "shared";
-import styles from "./page.module.css";
+import Image from 'next/image';
+import { Card } from 'shared';
+
+import styles from './page.module.css';
 
 function Gradient({
   conic,
   className,
-  small,
+  small
 }: {
   small?: boolean;
   conic?: boolean;
@@ -17,36 +18,36 @@ function Gradient({
         styles.gradient,
         conic ? styles.glowConic : undefined,
         small ? styles.gradientSmall : styles.gradientLarge,
-        className,
+        className
       ]
         .filter(Boolean)
-        .join(" ")}
+        .join(' ')}
     />
   );
 }
 
 const LINKS = [
   {
-    title: "Docs",
-    href: "https://turbo.build/repo/docs",
-    description: "Find in-depth information about Turborepo features and API.",
+    title: 'Docs',
+    href: 'https://turbo.build/repo/docs',
+    description: 'Find in-depth information about Turborepo features and API.'
   },
   {
-    title: "Learn",
-    href: "https://turbo.build/repo/docs/handbook",
-    description: "Learn more about monorepos with our handbook.",
+    title: 'Learn',
+    href: 'https://turbo.build/repo/docs/handbook',
+    description: 'Learn more about monorepos with our handbook.'
   },
   {
-    title: "Templates",
-    href: "https://turbo.build/repo/docs/getting-started/from-example",
-    description: "Choose from over 15 examples and deploy with a single click.",
+    title: 'Templates',
+    href: 'https://turbo.build/repo/docs/getting-started/from-example',
+    description: 'Choose from over 15 examples and deploy with a single click.'
   },
   {
-    title: "Deploy",
-    href: "https://vercel.com/new",
+    title: 'Deploy',
+    href: 'https://vercel.com/new',
     description:
-      " Instantly deploy your Turborepo to a shareable URL with Vercel.",
-  },
+      ' Instantly deploy your Turborepo to a shareable URL with Vercel.'
+  }
 ];
 
 export default function Page(): JSX.Element {
@@ -63,7 +64,7 @@ export default function Page(): JSX.Element {
             rel="noopener noreferrer"
             target="_blank"
           >
-            By{" "}
+            By{' '}
             <Image
               alt="Vercel Logo"
               className={styles.vercelLogo}
--- packages/shared/card.tsx
+++ packages/shared/card.tsx
@@ -1,10 +1,10 @@
-import * as React from "react";
+import * as React from 'react';
 
 export function Card({
   className,
   title,
   children,
-  href,
+  href
 }: {
   className?: string;
   title: string;
